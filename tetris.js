276047cfd389c394869b6329eba3292a307f1d4b
bouyass <lyesenjoythelife@hotmail.com>
2019-09-21 01:46:24 +0200

    try to modelize all pieces

--- tetris.js
+++ tetris.js
@@ -1,6 +1,13 @@
 
-var x = 10
-var y = 10
+var x1 = 10
+var x2 = 10
+var x3 = 10
+var x4 = 10
+
+var y1 = 10
+var y2 = 30
+var y3 = 50
+var y4 = 70
 
 const width = 20 
 const height = 20
@@ -17,13 +24,22 @@ ctx.fillStyle = 'green';
 
 
 
-function clearRect(){
-    ctx.clearRect(x,y,width,height)
-}
-
 function drawRect(){
 
-        ctx.fillRect(x,y,width,height)
+    ctx.fillRect(x1,y1,width,height)
+    ctx.fillRect(x2,y2,width,height)
+    ctx.fillRect(x3,y3,width,height)
+    ctx.fillRect(x4,y4,width,height)
+    ctx.lineWidth = 5;
+ctx.strokeStyle = "black";
+ctx.stroke()
+    
+
+}
+
+function clearRect(){
+
+        ctx.clearRect(x1,y1,width,height)
         console.log("y = "+ y)
 
 }
@@ -33,12 +49,12 @@ drawRect()
 
 
 
-setInterval(function(){
+/*setInterval(function(){
     if ( y !== 250){
     clearRect()
     y = y + 20
     drawRect()
     }
     
-},1000)
+},1000)*/
 
