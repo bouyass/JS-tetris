d33a9ecbe64ad611bfb5b49b7c0300c26aaeffd4
bouyass <lyesenjoythelife@hotmail.com>
2019-09-21 02:40:01 +0200

    piece modelizing finished

--- tetris.js
+++ tetris.js
@@ -1,8 +1,8 @@
 
-var x1 = 0
-var x2 = 0
-var x3 = 0
-var x4 = 0
+var x1 = 300
+var x2 = 300
+var x3 = 300
+var x4 = 300
 
 var y1 = 00
 var y2 = 20
@@ -22,11 +22,269 @@ var canvas = document.getElementById("can");
 
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'green';
 
 
+// this function will provide us the number corresponding to the next piece to draw 
+// taking into consideration the position of each piece we will have 19 number
+
+function randomPiece(){
+    pieceNumber = Math.floor(Math.random() * 18)
+    console.log("la prochaine pièce "+ pieceNumber)
+}
 
 function drawRect(){
+
+    switch(pieceNumber){
+        case 0:
+          x1 = 300
+          x2 = 300
+          x3 = 300
+          x4 = 300
+
+          y1 = 00
+          y2 = 20
+          y3 = 40
+          y4 = 60
+
+          ctx.fillStyle = 'green';
+        break
+        case 1:
+          x1 = 280
+          x2 = 300
+          x3 = 320
+          x4 = 340
+
+          y1 = 00
+          y2 = 00
+          y3 = 00
+          y4 = 00
+
+          ctx.fillStyle = 'green';
+        break
+        case 2:
+          x1 = 300
+          x2 = 300
+          x3 = 300
+          x4 = 280
+
+          y1 = 00
+          y2 = 20
+          y3 = 40
+          y4 = 40
+
+          ctx.fillStyle = 'red';
+        break
+        case 3:
+          x1 = 280
+          x2 = 280
+          x3 = 300
+          x4 = 320
+
+          y1 = 00
+          y2 = 20
+          y3 = 20
+          y4 = 20
+
+          ctx.fillStyle = 'red';
+        break
+        case 4:
+          x1 = 320
+          x2 = 300
+          x3 = 300
+          x4 = 300
+
+          y1 = 00
+          y2 = 00
+          y3 = 20
+          y4 = 40
+
+          ctx.fillStyle = 'red';
+        break
+        case 5:
+          x1 = 280
+          x2 = 300
+          x3 = 320
+          x4 = 320
+
+          y1 = 00
+          y2 = 00
+          y3 = 00
+          y4 = 20
+
+          ctx.fillStyle = 'red';
+        break
+        case 6:
+          x1 = 300
+          x2 = 300
+          x3 = 300
+          x4 = 320
+
+          y1 = 00
+          y2 = 20
+          y3 = 40
+          y4 = 40
+
+          ctx.fillStyle = 'yellow';
+        break
+        case 7:
+          x1 = 320
+          x2 = 300
+          x3 = 280
+          x4 = 280
+
+          y1 = 00
+          y2 = 00
+          y3 = 00
+          y4 = 20
+
+          ctx.fillStyle = 'yellow';
+        break
+        case 8:
+          x1 = 280
+          x2 = 300
+          x3 = 300
+          x4 = 300
+
+          y1 = 00
+          y2 = 00
+          y3 = 20
+          y4 = 40
+
+          ctx.fillStyle = 'yellow';
+        break
+        case 9:
+          x1 = 320
+          x2 = 320
+          x3 = 300
+          x4 = 280
+
+          y1 = 00
+          y2 = 20
+          y3 = 20
+          y4 = 20
+
+          ctx.fillStyle = 'yellow';
+        break
+        case 10:
+          x1 = 280
+          x2 = 300
+          x3 = 280
+          x4 = 300
+
+          y1 = 00
+          y2 = 00
+          y3 = 20
+          y4 = 20
+
+          ctx.fillStyle = 'pink';
+        break
+        case 11:
+          x1 = 320
+          x2 = 300
+          x3 = 300
+          x4 = 280
+
+          y1 = 00
+          y2 = 00
+          y3 = 20
+          y4 = 20
+
+          ctx.fillStyle = 'blue';
+        break
+        case 12:
+          x1 = 300
+          x2 = 300
+          x3 = 320
+          x4 = 320
+
+          y1 = 00
+          y2 = 20
+          y3 = 20
+          y4 = 40
+
+          ctx.fillStyle = 'blue';
+        break
+        case 13:
+          x1 = 280
+          x2 = 300
+          x3 = 300
+          x4 = 320
+
+          y1 = 00
+          y2 = 00
+          y3 = 20
+          y4 = 20
+
+          ctx.fillStyle = 'blue';
+        break
+        case 14:
+          x1 = 320
+          x2 = 320
+          x3 = 300
+          x4 = 280
+
+          y1 = 00
+          y2 = 20
+          y3 = 20
+          y4 = 20
+
+          ctx.fillStyle = 'blue';
+        break
+        case 15:
+          x1 = 280
+          x2 = 300
+          x3 = 320
+          x4 = 300
+
+          y1 = 00
+          y2 = 00
+          y3 = 00
+          y4 = 20
+
+          ctx.fillStyle = 'orange';
+        break
+        case 16:
+          x1 = 300
+          x2 = 300
+          x3 = 300
+          x4 = 280
+
+          y1 = 00
+          y2 = 20
+          y3 = 40
+          y4 = 20
+
+          ctx.fillStyle = 'orange';
+        break
+        case 17:
+          x1 = 280
+          x2 = 300
+          x3 = 320
+          x4 = 300
+
+          y1 = 20
+          y2 = 20
+          y3 = 20
+          y4 = 00
+
+          ctx.fillStyle = 'orange';
+        break
+        case 18:
+          x1 = 300
+          x2 = 300
+          x3 = 300
+          x4 = 320
+
+          y1 = 00
+          y2 = 20
+          y3 = 40
+          y4 = 20
+
+          ctx.fillStyle = 'orange';
+        break
+    
+        
+    }
 
     ctx.fillRect(x1,y1,width,height)
     ctx.fillRect(x2,y2,width,height)
@@ -47,19 +305,14 @@ function clearRect(){
 
 }
 
-pieceNumber()
+randomPiece()
 drawRect()
 
-// this function will provide us the number corresponding to the next piece to draw 
-// taking into consideration the position of each piece we will have 19 number
-
-function randomPiece(){
-    pieceNumber = Math.floor(Math.random() * 18)
-}
 
 
 
-setInterval(function(){
+
+/*setInterval(function(){
     
     if ( y4 < 400){
     clearRect()
@@ -70,5 +323,5 @@ setInterval(function(){
     drawRect()
     }
     
-},1000)
+},1000)*/
 
